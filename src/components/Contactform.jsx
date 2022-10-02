4662cdcc0621cae15ad0232c5e571c2c910fc068
julianluissoto <julianlasoto@hotmail.com>
2022-10-01 22:23:30 -0300

    contact form fixed

--- src/components/Contactform.jsx
+++ src/components/Contactform.jsx
@@ -1,8 +1,81 @@
 import React, {useState, useEffect} from 'react'
 import emailjs from'emailjs-com'
 import './contact.css'
+import styled from 'styled-components'
+
+const Container = styled.div`
+position: relative;
+
+height: 100%;
+display: flex;
+justify-content: center;
+align-items: center;
+padding: 100px 100px;
+margin: auto;
+
+`;
+const Form = styled.form`
+max-width: 500px;
+
+grid-template-columns: repeat(2, 1fr);
+justify-content: center;
+align-items: center;
+text-align: center;
+background-color: #fff;
+box-shadow: 0px 0px 19px 5px rgba(0, 0, 0, 0.19);
+`;
+
+const FormBody = styled.div`
+   padding: 25px 40px;
+
+`
+const InputForm   = styled.input`
+    width: 100%;
+    border: 2px solid rgba(0, 0, 0, 0);
+    outline: none;
+    background-color: rgba(230, 230, 230, 0.6);
+    padding: -1rem 1rem;
+    font-size: 1.1rem;
+    margin-top: 10px;
+    margin-bottom: 22px;
+    transition: .3s;
+
+    &:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+}
+
+`
+const FormTextArea = styled.textarea`
+   width: 100%;
+    border: 2px solid rgba(0, 0, 0, 0);
+    outline: none;
+    background-color: rgba(230, 230, 230, 0.6);
+    padding: -1rem 1rem;
+    font-size: 1.1rem;
+    margin-top: 10px;
+    margin-bottom: 22px;
+    transition: .3s;
+
+    &:hover {
+    background-color: rgba(0, 0, 0, 0.1)
+    }
+`
+const Button = styled.button`
+    width: 100%;
+    padding: 0.5rem 1rem;
+    background-color: #2ecc71;
+    color: #fff;
+    font-size: 1.1rem;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    transition: .3s;
 
 
+& :hover {
+    background-color: #27ae60;
+}
+`
 
 
 const Contactform = () => {
@@ -73,22 +146,25 @@ const Contactform = () => {
   return (
    
 
-<div className="container">
+<Container /* className="container" */>
 
-		<form className="contact-box" onSubmit={handleSubmit}>
+		<Form /* className="contact-box" */ onSubmit={handleSubmit}>
 			
-			<div className="right">
+			<FormBody /* className="right" */>
 				<h2 className='title'>Contact Me</h2>
-				<input  className='field' value={values.fullName||''}  onChange={handlechange} name="fullName" type="text" placeholder="Your name" />
-				<input className='field' value={values.email||''}   onChange={handlechange} name="email" type="email" placeholder="Your E-mail" />
+				<InputForm   value={values.fullName||''}  onChange={handlechange} name="fullName" type="text" placeholder="Your name" />
+				<InputForm   value={values.email||''}   onChange={handlechange} name="email" type="email" placeholder="Your E-mail" />
 				
 				
-				<textarea className="field" value = {values.message ||''}  onChange={handlechange} placeholder="Your message here" name="message" />
-				<button type="submit"   className="btn-submit">{values.sending?'Sending':'Send'}</button>
-                {status && renderAlert()}
-			</div>
-		</form  >
-	</div>
+				<FormTextArea  value = {values.message ||''}  onChange={handlechange} placeholder="Your message here" name="message" />
+				<Button type="submit" >
+        {values.sending?'Sending':'Send'}
+                </Button>
+        {status && renderAlert()}
+                
+			</FormBody>
+		</Form  >
+	</Container>
 
 
 
